a94b822fceecbcd3992b9cec8ad45f02cb02dbe8
Jai Kamat <jaipkamat@gmail.com>
2022-06-04 14:31:57 -0400

    chore: refine MultiSelect with MUI components

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,3 +1,16 @@
+import {
+    Button,
+    Checkbox,
+    FormControl,
+    InputLabel,
+    ListItemText,
+    MenuItem,
+    OutlinedInput,
+    Select,
+    SelectChangeEvent,
+    Stack,
+} from "@mui/material";
+
 interface Props {
     value: string[];
     onChange: (v: string[]) => void;
@@ -5,37 +18,41 @@ interface Props {
 }
 
 const MultiSelect: React.FC<Props> = ({ value, onChange, options }) => {
+    const handleChange = ({
+        target: { value },
+    }: SelectChangeEvent<string[]>) => {
+        onChange(typeof value === "string" ? value.split(",") : value);
+    };
+
     return (
-        <>
-            <div className="SelectContainer">
-                <select
-                    id="recipes"
-                    className="Select"
+        <Stack spacing={2} sx={{ width: 300 }}>
+            <FormControl sx={{ width: "inherit" }}>
+                <InputLabel id="multiple-select">Select</InputLabel>
+                <Select
+                    id="multiple-select"
+                    labelId="multiple-select"
                     multiple
                     value={value}
-                    onChange={(e) => {
-                        const val = Array.from(
-                            e.target.selectedOptions,
-                            (opt) => opt.value
-                        );
-                        onChange(val);
-                    }}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Select" />}
+                    renderValue={(selected) => selected.join(", ")}
                 >
-                    {options.map((option) => {
-                        return (
-                            <option value={option} key={option}>
-                                {option}
-                            </option>
-                        );
-                    })}
-                </select>
-            </div>
-            <div>
-                <button className="Button" onClick={() => onChange([])}>
-                    Reset
-                </button>
-            </div>
-        </>
+                    {options.map((option) => (
+                        <MenuItem key={option} value={option}>
+                            <Checkbox checked={value.includes(option)} />
+                            <ListItemText primary={option} />
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <Button
+                variant="outlined"
+                onClick={() => onChange([])}
+                sx={{ width: "inherit" }}
+            >
+                Reset
+            </Button>
+        </Stack>
     );
 };
 
