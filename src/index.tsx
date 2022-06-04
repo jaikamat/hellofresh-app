a9f0d190a17f8b84a30e69f9fd2a2c225d21c944
Jai Kamat <jaipkamat@gmail.com>
2022-06-04 17:04:55 -0400

    feat: use dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+    palette: { mode: "dark" },
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <CssBaseline>
-            <App />
-        </CssBaseline>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline>
+                <App />
+            </CssBaseline>
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
